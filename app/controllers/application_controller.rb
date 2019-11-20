8b87f088e719fd42c96d5418ee9fba0c7f1c772b
Ikaquan Bessee <ikaquanbessee@yahoo.com>
2019-11-20 07:48:31 -0500

    ok this is looking up

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,8 @@ require './config/environment'
 class ApplicationController < Sinatra::Base
 
   configure do
+    enable :sessions
+    set :session_secret, "welcome back"
     set :public_folder, 'public'
     set :views, 'app/views'
   end
@@ -10,5 +12,24 @@ class ApplicationController < Sinatra::Base
   get "/" do
     erb :welcome
   end
+ get "/login" do
+  erb :login
+ end
+ 
+ post "/login" do
+  @user =  User.find_by(email: params[:email])
+  if @user && @user.authenticate(params[:password])
+    session[:user_id] = @user.id
+    redirect "/tasks"
+  else 
+    redirect "/login"
+  end 
+end
+
+ 
+ get "/logout" do
+  session.clear
+  redirect "/" 
+ end 
 
 end
---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,40 @@
+class UsersController < ApplicationController
+
+  # GET: /users
+  get "/users" do
+    erb :"/users/index.html"
+  end
+
+  # GET: /users/new
+   get "/users/new" do  #making a Get request to /user/new. This is a route for the Signup Page
+    erb :"/users/new.html" #RENDING A FORM TO SAVE AN INSTANCE OF NEW USER 
+  end
+
+  # POST: /users
+  post "/signup" do #THis is where the params from the form end up.
+ user = User.create(params) #create a new user using the data the user put in (params)
+  redirect "/users/#{user.id}" #redirecting user to there show page using user id 
+  end
+
+  # GET: /users/5
+  get "/users/:id" do  #Show page Route
+  @user = User.find_by(id: params["id"])  
+  erb :"/users/show.html" 
+  end
+
+  # GET: /users/5/edit
+  get "/users/:id/edit" do
+    erb :"/users/edit.html"
+  end
+
+  # PATCH: /users/5
+  patch "/users/:id" do
+    redirect "/users/:id"
+  end
+
+  # DELETE: /users/5/delete
+  delete "/users/:id/delete" do
+    redirect "/users"
+  end
+end
+     
